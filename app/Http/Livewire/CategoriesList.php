601e4bdd824d57ed60e2fa95588eca288a967aeb
Ariful Islam Arif <devboyarif@gmail.com>
2023-04-16 14:49:02 +0600

    create category done

--- app/Http/Livewire/CategoriesList.php
+++ app/Http/Livewire/CategoriesList.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Category;
+use Illuminate\Support\Str;
 use Livewire\WithPagination;
 use Illuminate\Contracts\View\View;
 
@@ -11,9 +12,42 @@ class CategoriesList extends Component
 {
     use WithPagination;
 
+    public Category $category;
+
+    public bool $showModal = false;
+
+    public function openModal()
+    {
+        $this->showModal = true;
+
+        $this->category = new Category();
+    }
+
+    protected function rules(): array
+    {
+        return [
+            'category.name' => ['required', 'string', 'min:3'],
+            'category.slug' => ['nullable', 'string'],
+        ];
+    }
+
+    public function updatedCategoryName()
+    {
+        $this->category->slug = Str::slug($this->category->name);
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        $this->category->save();
+
+        $this->reset('showModal');
+    }
+
     public function render(): View
     {
-        $categories = Category::paginate(10);
+        $categories = Category::latest()->paginate(10);
 
         return view('livewire.categories-list', [
             'categories' => $categories,
